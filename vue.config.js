33cbe932889b69e7f256afb7fad8991fb4284c83
Qing/Nora <10qing14@gmail.com>
2023-01-24 16:42:32 -0800

    original version

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ],
-  publicPath: process.env.NODE_ENV === "production" ? "/Ricro-Calendar/" : "/",
+  ]
 })
