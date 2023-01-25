d725055981729a63a433ddc763fe21382bafe1e1
Qing/Nora <100110921+qingzzz@users.noreply.github.com>
2023-01-24 16:04:44 -0800

    Deploy Vue App

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify',
-    publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/Ricro-Calendar/" : "/",
 
   ]
 })
